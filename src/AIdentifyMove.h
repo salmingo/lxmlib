006f23bb4b425ee52ef8e3da697adaf3e7fad110
salmingo <lxm@nao.cas.cn>
2017-12-03 22:29:20 +0800

    171202
    trivial

--- src/AIdentifyMove.h
+++ src/AIdentifyMove.h
@@ -8,6 +8,7 @@
 #ifndef AIDENTIFYMOVE_H_
 #define AIDENTIFYMOVE_H_
 
+#include <vector>
 #include "ADefine.h"
 
 namespace AstroUtil {
@@ -20,12 +21,23 @@ public:
 public:
 	/* 数据结构 */
 	/*!
-	 * @class 帧目标
+	 * @struct 单个瞬变源
 	 */
-	typedef struct frame_objects {
-		int  n;		//< 目标数量
-		PT3F *pts;	//< 目标位置集合
-	} * ptr_frmobj;
+	typedef struct ObservedTransient {
+		double x, y;		//< XY坐标
+		double ra, dc;	//< 赤经/赤纬, 量纲: 角度
+		double mag;		//< 星等
+		int type;
+	} OT, * PtrOT;
+	typedef std::vector<OT> OTVec;
+
+	/*!
+	 * @struct 单幅图像中的所有瞬变源
+	 */
+	typedef struct FrameObservedTransient {
+		double secs;		//< 图像对应的当日秒数
+		OTVec ots;		//< 图像中瞬变源集合
+	} FrmOT, * PtrFrmOT;
 
 protected:
 	/* 成员变量 */
